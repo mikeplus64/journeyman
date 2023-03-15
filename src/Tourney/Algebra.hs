408d93bf61957bc9191d31f95498ed73f31ffbf2
Mike Ledger <mike@quasimal.com>
2023-03-15 22:59:33 +1100

    cabal

--- src/Tourney/Algebra.hs
+++ src/Tourney/Algebra.hs
@@ -1,1 +1,13 @@
 module Tourney.Algebra where
+
+newtype PlayerID = PlayerID Int
+newtype MatchID = MatchID Int
+
+data Player
+  = LoserOf MatchId
+  | WinnerOf MatchId
+  | Seed Int
+
+data T
+  = Match MatchID Player
+  | Connect T T
