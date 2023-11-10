2db8113eeb01e25864afda4e4bb8cadaf55e81eb
Mike Ledger <eleventynine@gmail.com>
2023-11-10 13:46:43 +1100

    writer

--- src/Tourney/Algebra/Builder.hs
+++ src/Tourney/Algebra/Builder.hs
@@ -18,14 +18,14 @@ import Tourney.Match
 import Tourney.Types
 import Prelude hiding (round, sequence)
 
-newtype Builder t r a = Builder (ContT r (AccumT (Tournament t) DList) a)
-  deriving (Functor, Applicative, Monad) via (ContT r (AccumT (Tournament t) DList))
+newtype Builder t a = Builder (Writer (Tournament t) a)
+  deriving (Functor, Applicative, Monad, MonadWriter (Tournament t)) via (Writer (Tournament t))
 
-tellBuilder :: Tournament t -> Builder t r ()
-tellBuilder t = Builder (lift (add t))
+tellBuilder :: Tournament t -> Builder t ()
+tellBuilder t = Builder (tell t)
 
-runBuilder :: (a -> r) -> Builder t r a -> [(r, Tournament t)]
-runBuilder f b = DL.toList (runBuilderDL f b)
+runBuilder :: Builder t a -> (a, Tournament t)
+runBuilder (Builder b) = runWriter b
 
 --------------------------------------------------------------------------------
 -- Steps
@@ -34,38 +34,38 @@ runBuilder f b = DL.toList (runBuilderDL f b)
 -- monad create rounds. See 'Round' for a builder monad to create rounds.
 type Steps = Builder TMany
 
-runSteps :: (a -> r) -> Steps r a -> ([r], Tournament TMany)
-runSteps f = second U.sequence . unzip . runBuilder f
+runSteps :: Steps a -> (a, Tournament TMany)
+runSteps = runBuilder
 
-execSteps :: Steps a a -> Tournament TMany
-execSteps = U.sequence . map snd . runBuilder id
+execSteps :: Steps a -> Tournament TMany
+execSteps = snd . runBuilder
 
-tellSteps :: Tournament TMany -> Steps a ()
-tellSteps t = Builder (lift (add t))
+tellSteps :: Tournament TMany -> Steps ()
+tellSteps t = Builder (tell t)
 
-class AsSteps a x | a -> x where toSteps :: a -> Steps x x
-instance x ~ y => AsSteps (Steps x y) x where toSteps = id
+class AsSteps a x | a -> x where toSteps :: a -> Steps x
+instance AsSteps (Steps x) x where toSteps = id
 instance AsSteps a () => AsSteps [a] () where toSteps = mapM_ (toSteps @a)
 
 -- Basic syntax for 'Steps'
 ----------------------------------------
 
 -- | Add a round to steps, giving the resulting values
-round :: Round a a -> Steps s [a]
-round r = as <$ tellSteps (LiftTMany t)
+round :: Round a -> Steps a
+round r = a <$ tellSteps (LiftTMany t)
   where
-    (as, t) = runRound id r
+    (a, t) = runRound r
 
 -- | Add a round to steps
-round_ :: Round a a -> Steps s ()
+round_ :: Round a -> Steps ()
 round_ = void . round
 
 -- | Add some rounds to steps
-rounds_ :: [Round x x] -> Steps s ()
+rounds_ :: [Round x] -> Steps ()
 rounds_ = mapM_ round_
 
-class AsRound a x | a -> x where toRound :: a -> Round x x
-instance x ~ y => AsRound (Round x y) x where toRound = id
+class AsRound a x | a -> x where toRound :: a -> Round x
+instance AsRound (Round x) x where toRound = id
 instance AsRound Match () where toRound = match
 instance AsRound a () => AsRound [a] () where
   toRound = overlays_ . map (toRound @a) . toList
@@ -78,11 +78,11 @@ instance AsRound a () => AsRound [a] () where
 -- 'U.overlay'.
 type Round = Builder TOne
 
-runRound :: (a -> r) -> Round r a -> ([r], Tournament TOne)
-runRound f = second U.overlay . unzip . runBuilder f
+runRound :: Round a -> (a, Tournament TOne)
+runRound = runBuilder
 
-tellRound :: Tournament TOne -> Round a ()
-tellRound t = Builder (lift (add t))
+tellRound :: Tournament TOne -> Round ()
+tellRound t = Builder (tell t)
 
 -- Basic syntax for 'Round'
 ----------------------------------------
@@ -92,85 +92,76 @@ instance (x ~ Int, y ~ Int) => AsMatch (x, y) where toMatch = uncurry OrdPair_
 instance AsMatch Match where toMatch = id
 
 -- | Add a match
-match :: AsMatch m => m -> Round r ()
+match :: AsMatch m => m -> Round ()
 match m = tellRound (One (toMatch m))
 
 --------------------------------------------------------------------------------
 -- Shared syntax for both
 
--- | Lift a list into a 'Round' for iteration. The underlying effect on the
--- 'Round' is to overlay the iterated elements
-list :: Foldable f => f a -> Builder t r a
-list a = Builder (lift (lift (foldr DL.cons DL.empty a)))
+-- | Overlay a collection of builders
+overlays :: Foldable f => f (Builder t a) -> Builder t [a]
+overlays bs =
+  Builder
+    . writer
+    . second U.overlay
+    $ unzip (map runBuilder (toList bs))
 
 -- | Overlay a collection of builders
-overlaysBy :: Foldable f => (a -> x) -> f (Builder t x a) -> Builder t r [x]
-overlaysBy f b = Builder $ ContT \ret -> do
-  let r = map (runBuilder f) (toList b)
-  add (U.overlay (concatMap (map snd) r))
-  ret (concatMap (map fst) r)
-
--- | Overlay a collection of builders
-overlays :: Foldable f => f (Builder t a a) -> Builder t r [a]
-overlays = overlaysBy id
-
--- | Overlay a collection of builders
-overlays_ :: Foldable f => f (Builder t a a) -> Builder t r ()
+overlays_ :: Foldable f => f (Builder t a) -> Builder t ()
 overlays_ = void . overlays
 
--- | Retrieve the current standings
---
--- Warning: this operation will have the effect of segmenting the tournament
--- into two sections, as a tournament runner cannot continue past this point
--- without first having the standings.
-standings :: Builder t () Standings
-standings = Builder $ ContT \f -> do
-  add (ByStandings \s -> overlayAccumT (f s))
+-- -- | Retrieve the current standings
+-- --
+-- -- Warning: this operation will have the effect of segmenting the tournament
+-- -- into two sections, as a tournament runner cannot continue past this point
+-- -- without first having the standings.
+-- standings :: Builder t () Standings
+-- standings = Builder $ ContT \f -> do
+--   add (ByStandings \s -> overlayAccumT (f s))
 
--- | Retrieve the current focus
-focus :: Builder t () Focus
-focus = Builder $ ContT \f -> do
-  add (ByFocus \s -> overlayAccumT (f s))
+-- -- | Retrieve the current focus
+-- focus :: Builder t () Focus
+-- focus = Builder $ ContT \f -> do
+--   add (ByFocus \s -> overlayAccumT (f s))
 
--- | Retrieve the current player count
-playerCount :: Builder t () PlayerCount
-playerCount = Builder $ ContT \f -> do
-  add (ByPlayerCount \s -> overlayAccumT (f s))
+-- -- | Retrieve the current player count
+-- playerCount :: Builder t () PlayerCount
+-- playerCount = Builder $ ContT \f -> do
+--   add (ByPlayerCount \s -> overlayAccumT (f s))
 
 -- Combinators
 ---------------
 
--- | Set the focus of the inner builder
-withFocus :: Merge t => Int -> Int -> Builder t a a -> Builder t r ()
-withFocus focusStart focusLength = withFocii (const [Focus{focusStart, focusLength}])
+-- -- | Set the focus of the inner builder
+-- withFocus :: Merge t => Int -> Int -> Builder t a a -> Builder t ()
+-- withFocus focusStart focusLength = withFocii (const [Focus{focusStart, focusLength}])
 
--- | Set the focii of the inner builder
-withFocii :: Merge t => (Focus -> [Focus]) -> Builder t a a -> Builder t r ()
-withFocii makeFocii b = Builder do
-  let (_as, ts) = unzip (runBuilder id b)
-  lift (add (Modify (SetFocus makeFocii) (merge ts)))
+-- -- | Set the focii of the inner builder
+-- withFocii :: Merge t => (Focus -> [Focus]) -> Builder t a a -> Builder t ()
+-- withFocii makeFocii b = Builder do
+--   let (_as, ts) = unzip (runBuilder id b)
+--   lift (add (Modify (SetFocus makeFocii) (merge ts)))
 
--- | Set the sort method of the inner builder
-withSortMethod :: Merge t => SortMethod -> Builder t a a -> Builder t r ()
-withSortMethod method b = Builder do
-  let (_as, ts) = unzip (runBuilder id b)
-  lift (add (Modify (SetSortMethod method) (merge ts)))
+-- -- | Set the sort method of the inner builder
+-- withSortMethod :: Merge t => SortMethod -> Builder t a a -> Builder t ()
+-- withSortMethod method b = Builder do
+--   let (_as, ts) = unzip (runBuilder id b)
+--   lift (add (Modify (SetSortMethod method) (merge ts)))
 
--- | Set the offset of the inner builder
-withOffset :: Merge t => Int -> Builder t a a -> Builder t r ()
-withOffset offs b = Builder do
-  let (_as, ts) = unzip (runBuilder id b)
-  lift (add (Modify (SetOffset offs) (merge ts)))
+-- -- | Set the offset of the inner builder
+-- withOffset :: Merge t => Int -> Builder t a a -> Builder t ()
+-- withOffset offs b = Builder do
+--   let (_as, ts) = unzip (runBuilder id b)
+--   lift (add (Modify (SetOffset offs) (merge ts)))
 
 -- | Given a denominator @n@ and a builder, split the tournament into @n@
 -- sub-tournaments and run the builder on each
-divideInto :: Merge t => Int -> Builder t () () -> Builder t () ()
-divideInto denom builder = do
-  count <- playerCount
+divideInto :: Merge t => Int -> Builder TMany () -> Builder TMany ()
+divideInto denom builder = tellBuilder $ ByPlayerCount \count -> do
   let (m, r) = quotRem count denom
   let groupSize = m + r
-  overlays_
-    [ withFocus (groupNo * groupSize) groupSize builder
+  U.overlay
+    [ Modify (SetFocus (\_ -> [Focus (groupNo * groupSize) groupSize])) (execSteps builder)
     | groupNo <- [0 .. denom - 1]
     ]
 
@@ -210,15 +201,6 @@ collapseMatches midpoint players = take midpoint (zipWith OrdPair_ players (reve
 
 --------------------------------------------------------------------------------
 -- Internals
-
-overlayAccumT :: AccumT (Tournament t) DList x -> Tournament t
-overlayAccumT a =
-  trace "overlayAccumT" $
-    U.overlay . DL.toList . flip execAccumT mempty $
-      a
-
-runBuilderDL :: (a -> r) -> Builder t r a -> DList (r, Tournament t)
-runBuilderDL f (Builder run) = runAccumT (runContT run (\x -> pure (f x))) mempty
 
 class Merge (t :: Depth) where merge :: [Tournament t] -> Tournament t
 instance Merge TOne where merge = U.overlay
